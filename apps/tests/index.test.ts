4f4551760ef6fb168bd18d992de214d4fd981d9a
DevDesignAmitesh <amiteshsingh252@gmail.com>
2026-04-15 14:59:58 +0530

    added admin route tests

--- apps/tests/index.test.ts
+++ apps/tests/index.test.ts
@@ -2,7 +2,6 @@
 // using global variables rather than functions
 // conuning with other tests
 
-
 import { describe, it, expect } from "bun:test";
 import axios from "axios";
 
@@ -11,6 +10,8 @@ type role = "USER" | "ADMIN";
 const HTTP_URL = "http://localhost:4000/api/v1";
 
 let elementId = "";
+let avatarId = "";
+const fakeElementId = "d4f3588f-11b3-416a-9771-a405a243ce5c";
 
 async function createUser(role: role) {
   const data = {
@@ -39,6 +40,9 @@ async function createAvatar(token: string) {
 
   return { id: res.data.id };
 }
+
+const admin = await createUser("ADMIN");
+const user = await createUser("USER");
 
 describe("POST /signup for ADMIN", () => {
   it("it should succeed", async () => {
@@ -72,9 +76,7 @@ describe("POST /signup for ADMIN", () => {
   });
 
   it("it should give admin already exists", async () => {
-    const data = await createUser("ADMIN");
-
-    const res = await axios.post(`${HTTP_URL}/signup`, data, {
+    const res = await axios.post(`${HTTP_URL}/signup`, admin, {
       validateStatus: () => true,
     });
 
@@ -114,9 +116,7 @@ describe("POST /signup for USER", () => {
   });
 
   it("it should give user already exists", async () => {
-    const data = await createUser("USER");
-
-    const res = await axios.post(`${HTTP_URL}/signup`, data, {
+    const res = await axios.post(`${HTTP_URL}/signup`, user, {
       validateStatus: () => true,
     });
 
@@ -126,9 +126,7 @@ describe("POST /signup for USER", () => {
 
 describe("POST /signin for ADMIN", () => {
   it("it should succeed", async () => {
-    const data = await createUser("ADMIN");
-
-    const res = await axios.post(`${HTTP_URL}/signin`, data, {
+    const res = await axios.post(`${HTTP_URL}/signin`, admin, {
       validateStatus: () => true,
     });
     expect(res.data.token).toBeDefined();
@@ -168,9 +166,7 @@ describe("POST /signin for ADMIN", () => {
 
 describe("POST /signin for USER", () => {
   it("it should succeed", async () => {
-    const data = await createUser("USER");
-
-    const res = await axios.post(`${HTTP_URL}/signin`, data, {
+    const res = await axios.post(`${HTTP_URL}/signin`, user, {
       validateStatus: () => true,
     });
     expect(res.data.token).toBeDefined();
@@ -210,11 +206,9 @@ describe("POST /signin for USER", () => {
 
 describe("GET /me", () => {
   it("it should be successfull", async () => {
-    const { token } = await createUser("ADMIN");
-
     const res = await axios.get(`${HTTP_URL}/me`, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${user.token}`,
       },
       validateStatus: () => true,
     });
@@ -237,20 +231,254 @@ describe("GET /me", () => {
   });
 });
 
+describe("GET /avatars", () => {
+  it("it should be successfull", async () => {
+    const res = await axios.get(`${HTTP_URL}/avatars`, {
+      validateStatus: () => true,
+    });
+
+    expect(res.data.avatars).toBeArray();
+  });
+});
+
+// ids: string[]
+describe("GET /user/metadata/bulk?ids", () => {
+  it("it should be successfull", async () => {
+    const res = await axios.get(
+      `${HTTP_URL}/user/metadata/bulk?ids=[${user.id}, ${admin.id}]`,
+      {
+        validateStatus: () => true,
+      },
+    );
+
+    expect(res.data.avatars).toBeArray();
+  });
+});
+
+describe("POST /admin/element", () => {
+  it("it should succeed", async () => {
+    const data = {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+      width: 1,
+      height: 1,
+      static: true,
+    };
+
+    const res = await axios.post(`${HTTP_URL}/admin/element`, data, {
+      headers: {
+        Authorization: `Bearer ${admin.token}`,
+      },
+    });
+
+    expect(res.data.id).toBeDefined();
+    elementId = res.data.id;
+  });
+
+  it("it should fail as the user is not admin", async () => {
+    const data = {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+      width: 1,
+      height: 1,
+      static: true,
+    };
+
+    const res = await axios.post(`${HTTP_URL}/admin/element`, data, {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+
+    expect(res.status).toBe(409);
+  });
+
+  it("it should fail as the inputs are invalid", async () => {
+    const data = {
+      imageUrl: "heloo",
+      width: "1",
+      height: "1",
+      static: "choco",
+    };
+
+    const res = await axios.post(`${HTTP_URL}/admin/element`, data, {
+      headers: {
+        Authorization: `Bearer ${admin.token}`,
+      },
+    });
+
+    expect(res.status).toBe(400);
+  });
+
+  it("it should fail as un-authorized", async () => {
+    const data = {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+      width: 1,
+      height: 1,
+      static: true,
+    };
+
+    const res = await axios.post(`${HTTP_URL}/admin/element`, data, {
+      headers: {
+        Authorization: `Bearer choco`,
+      },
+    });
+
+    expect(res.status).toBe(401);
+  });
+});
+
+describe("PUT /admin/element/:elementId", () => {
+  it("it should succeed", async () => {
+    const data = {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+    };
+
+    const res = await axios.put(
+      `${HTTP_URL}/admin/element/${elementId}`,
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${admin.token}`,
+        },
+      },
+    );
+
+    expect(res.data.id).toBeDefined();
+  });
+
+  it("it will fail as user cant update this", async () => {
+    const data = {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+    };
+
+    const res = await axios.put(
+      `${HTTP_URL}/admin/element/${elementId}`,
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      },
+    );
+
+    expect(res.data.id).toBeDefined();
+  });
+
+  it("it will fail as element will not be found", async () => {
+    const data = {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+    };
+
+    const res = await axios.put(
+      `${HTTP_URL}/admin/element/${fakeElementId}`,
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${admin.token}`,
+        },
+      },
+    );
+
+    expect(res.data.id).toBeDefined();
+  });
+
+  it("it will fail as invalid inputs", async () => {
+    const data = {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+    };
+
+    const res = await axios.put(`${HTTP_URL}/admin/element/choco`, data, {
+      headers: {
+        Authorization: `Bearer ${admin.token}`,
+      },
+    });
+
+    expect(res.data.id).toBeDefined();
+  });
+});
+
+describe("POST /admin/avatar", () => {
+  it("it should succeed", async () => {
+    const data = {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
+      name: "Timmy",
+    };
+
+    const res = await axios.post(`${HTTP_URL}/admin/avatar`, data, {
+      headers: {
+        Authorization: `Bearer ${admin.token}`,
+      },
+    });
+
+    expect(res.data.avatarId).toBeDefined();
+    avatarId = res.data.avatarId;
+  });
+
+  it("it should fail as the user is not admin", async () => {
+    const data = {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
+      name: "Timmy",
+    };
+
+    const res = await axios.post(`${HTTP_URL}/admin/avatar`, data, {
+      headers: {
+        Authorization: `Bearer ${user.token}`,
+      },
+    });
+
+    expect(res.status).toBe(409);
+  });
+
+  it("it should fail as the inputs are invalid", async () => {
+    const data = {
+      imageUrl: "chcoc",
+      name: "Timmy",
+    };
+
+    const res = await axios.post(`${HTTP_URL}/admin/avatar`, data, {
+      headers: {
+        Authorization: `Bearer ${admin.token}`,
+      },
+    });
+
+    expect(res.status).toBe(400);
+  });
+
+  it("it should fail as un-authorized", async () => {
+    const data = {
+      imageUrl:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQm3RFDZM21teuCMFYx_AROjt-AzUwDBROFww&s",
+      name: "Timmy",
+    };
+
+    const res = await axios.post(`${HTTP_URL}/admin/avatar`, data, {
+      headers: {
+        Authorization: `Bearer choco`,
+      },
+    });
+
+    expect(res.status).toBe(401);
+  });
+});
+
 describe("PUT /user/metadata", () => {
   it("it should be successfull", async () => {
-    const { token } = await createUser("ADMIN");
-    const { id } = await createAvatar(token);
-    const { token: t2 } = await createUser("USER");
-
     const res = await axios.put(
       `${HTTP_URL}/user/metadata`,
       {
-        avatarId: id,
+        avatarId,
       },
       {
         headers: {
-          Authorization: `Bearer ${t2}`,
+          Authorization: `Bearer ${user.token}`,
         },
         validateStatus: () => true,
       },
@@ -260,8 +488,6 @@ describe("PUT /user/metadata", () => {
   });
 
   it("it should say invalid inputs", async () => {
-    const { token } = await createUser("ADMIN");
-
     const res = await axios.put(
       `${HTTP_URL}/user/metadata`,
       {
@@ -269,7 +495,7 @@ describe("PUT /user/metadata", () => {
       },
       {
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${user.token}`,
         },
         validateStatus: () => true,
       },
@@ -296,106 +522,91 @@ describe("PUT /user/metadata", () => {
   });
 });
 
-describe("GET /avatars", () => {
-  it("it should be successfull", async () => {
-    const res = await axios.get(`${HTTP_URL}/avatars`, {
-      validateStatus: () => true,
-    });
-
-    expect(res.data.avatars).toBeArray();
-  });
-});
-
-// ids: string[]
-describe("GET /user/metadata/bulk?ids", () => {
-  it("it should be successfull", async () => {
-    const { id: id1 } = await createUser("USER");
-    const { id: id2 } = await createUser("USER");
-
-    const res = await axios.get(
-      `${HTTP_URL}/user/metadata/bulk?ids=[${id1}, ${id2}]`,
-      {
-        validateStatus: () => true,
-      },
-    );
-
-    expect(res.data.avatars).toBeArray();
-  });
-});
-
-describe("POST /admin/element", () => {
+describe("/POST /admin/map", () => {
   it("it should succeed", async () => {
-    const { token } = await createUser("ADMIN");
-
     const data = {
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
-      width: 1,
-      height: 1,
-      static: true,
+      thumbnail: "https://thumbnail.com/a.png",
+      dimensions: "100x200",
+      name: "100 person interview room",
+      defaultElements: [
+        {
+          elementId,
+          x: 20,
+          y: 20,
+        },
+      ],
     };
 
-    const res = await axios.post(`${HTTP_URL}/admin/element`, data, {
+    const res = await axios.post(`${HTTP_URL}/admin/map`, data, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${admin.token}`,
       },
     });
 
     expect(res.data.id).toBeDefined();
-    elementId = res.data.id;
   });
 
-  it("it should fail as the user is not admin", async () => {
-    const { token } = await createUser("USER");
-
+  it("it will fail as user cant create maps", async () => {
     const data = {
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
-      width: 1,
-      height: 1,
-      static: true,
+      thumbnail: "https://thumbnail.com/a.png",
+      dimensions: "100x200",
+      name: "100 person interview room",
+      defaultElements: [
+        {
+          elementId,
+          x: 20,
+          y: 20,
+        },
+      ],
     };
 
-    const res = await axios.post(`${HTTP_URL}/admin/element`, data, {
+    const res = await axios.post(`${HTTP_URL}/admin/map`, data, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${user.token}`,
       },
     });
 
     expect(res.status).toBe(409);
   });
 
-  it("it should fail as the inputs are invalid", async () => {
-    const { token } = await createUser("ADMIN");
-
+  it("it will fail as invalid inputs", async () => {
     const data = {
-      imageUrl:
-        "heloo",
-      width: "1",
-      height: "1",
-      static: "choco",
+      thumbnail: "choco",
+      dimensions: "100x200",
+      name: "100 person interview room",
+      defaultElements: [
+        {
+          elementId,
+          x: 20,
+          y: 20,
+        },
+      ],
     };
 
-    const res = await axios.post(`${HTTP_URL}/admin/element`, data, {
+    const res = await axios.post(`${HTTP_URL}/admin/map`, data, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${admin.token}`,
       },
     });
 
     expect(res.status).toBe(400);
   });
 
-  
-  it("it should fail as the admin in un-authorized", async () => {
+  it("it will fail as un-authorized", async () => {
     const data = {
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
-      width: 1,
-      height: 1,
-      static: true,
+      thumbnail: "choco",
+      dimensions: "100x200",
+      name: "100 person interview room",
+      defaultElements: [
+        {
+          elementId,
+          x: 20,
+          y: 20,
+        },
+      ],
     };
 
-    const res = await axios.post(`${HTTP_URL}/admin/element`, data, {
+    const res = await axios.post(`${HTTP_URL}/admin/map`, data, {
       headers: {
         Authorization: `Bearer choco`,
       },
@@ -403,58 +614,29 @@ describe("POST /admin/element", () => {
 
     expect(res.status).toBe(401);
   });
-});
 
-
-describe("POST /admin/element/:elementId", () => {
-  it("it should succeed", async () => {
-    const { token } = await createUser("ADMIN");
+  it("it will fail as element will not be found", async () => {
+    const fakeElementId = "d4f3588f-11b3-416a-9771-a405a243ce5c";
 
     const data = {
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
+      thumbnail: "choco",
+      dimensions: "100x200",
+      name: "100 person interview room",
+      defaultElements: [
+        {
+          elementId: fakeElementId,
+          x: 20,
+          y: 20,
+        },
+      ],
     };
 
-    const res = await axios.post(`${HTTP_URL}/admin/element/:${elementId}`, data, {
+    const res = await axios.post(`${HTTP_URL}/admin/map`, data, {
       headers: {
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${admin.token}`,
       },
     });
 
-    expect(res.data.id).toBeDefined();
-  });
-
-  it("it should succeed", async () => {
-    const { token } = await createUser("USER");
-
-    const data = {
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
-    };
-
-    const res = await axios.post(`${HTTP_URL}/admin/element/:${elementId}`, data, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    expect(res.data.id).toBeDefined();
-  });
-
-  it("it should succeed", async () => {
-    const { token } = await createUser("ADMIN");
-
-    const data = {
-      imageUrl:
-        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRCRca3wAR4zjPPTzeIY9rSwbbqB6bB2hVkoTXN4eerXOIkJTG1GpZ9ZqSGYafQPToWy_JTcmV5RHXsAsWQC3tKnMlH_CsibsSZ5oJtbakq&usqp=CAE",
-    };
-
-    const res = await axios.post(`${HTTP_URL}/admin/element/:${elementId}`, data, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    expect(res.data.id).toBeDefined();
+    expect(res.status).toBe(404);
   });
 });
